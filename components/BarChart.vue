edb304e436080dec6a07fa460ea07378ade05ec3
DavidWants2Learn <David.Oh.331@my.csun.edu>
2019-08-09 13:01:52 -0700

    Bar Chart Works

--- components/BarChart.vue
+++ components/BarChart.vue
@@ -19,132 +19,51 @@ import LACityParkingData from '~/components/LACityParkingData.vue';
     components: {
       LACityParkingData
     },
-    // methods: {
-    //   makeVis(crimeData) {
-    //     var margin = { top: 30, right: 50, bottom: 30, left: 50 },
-    //         width  = 550 - margin.left - margin.right,
-    //         height = 250 - margin.top  - margin.bottom;
-        
-    //     var xScale = d3.scale.ordinal()
-    //         .domain(months)
-    //         .rangeRoundBands([0, width], 0.1);
+    methods: {
 
-    //     var yScale = d3.scale.linear()
-    //         .range([height, 0]);
-        
-    //     var canvas = d3.select("#vis-container")
-    //         .append("svg")
-    //           .attr("width",  width  + margin.left + margin.right)
-    //           .attr("height", height + margin.top  + margin.bottom)
-    //         .append("g")
-    //           .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    //     var yAxis = d3.svg.axis()
-    //         .scale(yScale)
-    //         .orient("left");
-
-    //     var yAxisHandleForUpdate = canvas.append("g")
-    //         .attr("class", "y axis")
-    //         .call(yAxis);
-
-    //     yAxisHandleForUpdate.append("text")
-    //         .attr("transform", "rotate(-90)")
-    //         .attr("y", 6)
-    //         .attr("dy", ".71em")
-    //         .style("text-anchor", "end")
-    //         .text("Value");
-
-    //     var updateBars = function(data) {
-    //         // First update the y-axis domain to match data
-    //         yScale.domain( d3.extent(data) );
-    //         yAxisHandleForUpdate.call(yAxis);
-
-    //     var bars = canvas.selectAll(".bar").data(data);
-
-    //     // Add bars for new data
-    //     bars.enter()
-    //         .append("rect")
-    //         .attr("class", "bar")
-    //         .attr("x", function(d,i) { return xScale( months[i] ); })
-    //         .attr("width", xScale.rangeBand())
-    //         .attr("y", function(d,i) { return yScale(d); })
-    //         .attr("height", function(d,i) { return height - yScale(d); });
-
-    //     // Update old ones, already have x / width from before
-    //     bars
-    //         .transition().duration(250)
-    //         .attr("y", function(d,i) { return yScale(d); })
-    //         .attr("height", function(d,i) { return height - yScale(d); });
-
-    //     // Remove old ones
-    //     bars.exit().remove();
-
-    //     // Get names of cereals, for dropdown
-    //     var cereals = Object.keys(crimeData).sort();
-
-    //     var dropdown = d3.select("#vis-container")
-    //         .insert("select", "svg")
-    //         .on("change", dropdownChange);
-
-    //     dropdown.selectAll("option")
-    //         .data(cereals)
-    //         .enter().append("option")
-    //         .attr("value", function (d) { return d; })
-    //         .text(function (d) {
-    //             return d[0].toUpperCase() + d.slice(1,d.length); // capitalize 1st letter
-    //         });
-
-    //     var initialData = crimeData[ crime[0] ];
-    //     updateBars(initialData);
-    //     };
-    //   },
-    // },
+    },
     mounted() {
+      var margin = {top: 50, right: 50, bottom: 50, left: 50};
+      var width = 600 - margin.left - margin.right;
+      var height = 600 - margin.top - margin.bottom;
+
+      var svg = d3.select("#bar-chart").append('svg')
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      var x = d3.scaleBand().range([0, width]);
+      var y = d3.scaleLinear().range([height, 0]);
+
       eventBus.$on('LA_CrimeData', data => {
-        var crimeType = {};
-        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
         console.log(data);
-
-        // Counts number of arrests by age.
-        var countAges = alasql('SELECT INDEX age, COUNT(*) AS cnt FROM ? GROUP BY age', [data]);
-        // Counts area where arrest was made
-        var countArea = alasql('SELECT INDEX area_desc, COUNT(*) AS cnt FROM ? GROUP BY area_desc', [data]);
-        // Counts type of crime ie, dependant (d), felony (f), infraction (i), misdemeanor (m), other (o)
-        var countCrimeType = alasql('SELECT INDEX arst_typ_cd, COUNT(*) AS cnt FROM ? GROUP BY arst_typ_cd', [data]);
-
-        // counts Crime Description
-        var countChargeDesc = alasql('SELECT INDEX chrg_desc, COUNT(*) AS cnt FROM ? GROUP BY chrg_desc', [data]);
-
-        // Counts race ethnicity, origin
-        var countDemographics = alasql('SELECT INDEX descent_cd, COUNT(*) AS cnt FROM ? GROUP BY descent_cd', [data]);
-        
-        // Counts Crime by Category
-        var countCrimeByGroupDesc = alasql('SELECT INDEX grp_description, COUNT(*) AS cnt FROM ? GROUP BY grp_description', [data]);
-        
         // Counts Sex (Male / Female)
         var countSex = alasql('SELECT INDEX sex_cd, COUNT(*) AS cnt FROM ? GROUP BY sex_cd', [data]);
-        
-        // Counts time of arrest, **Look into grouping by category
-        var countTime = alasql('SELECT INDEX time, COUNT(*) AS cnt FROM ? GROUP BY time', [data]);
 
-        console.log(countAges);
-        console.log(countArea);
-        console.log(countCrimeType);
-        console.log(countChargeDesc);
-        console.log(countDemographics);
-        console.log(countGroupDesc);
         console.log(countSex);
-        console.log(countTime);
 
-        var crimeData = data.forEach(function(d) {
-          var crime = d.chrg_desc;
-          crimeType[crime] = [];
+        x.domain(data.map(function(d) { return d.arst_typ_cd }));
+        y.domain([0, d3.max(data, function(d) { return d.age; })]);
 
-          months.forEach(function(month) {
-            crimeType[crime].push( +[month])
-          });
-        });
-      })
+        // function creates bar charts using rect
+        svg.selectAll(".bar")
+          .data(data)
+          .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.arst_typ_cd); })
+          .attr("width", x.bandwidth())
+          .attr("y", function(d) { return y(d.age); })
+          .attr("height", function(d) { return height - y(d.age); });
+
+        svg.append("g")
+          .attr("transform", "translate(0," + height + ")")
+          .call(d3.axisBottom(x));
+
+        svg.append("g")
+          .call(d3.axisLeft(y));
+
+      });
     }
   }
 </script>
@@ -152,16 +71,3 @@ import LACityParkingData from '~/components/LACityParkingData.vue';
 <style>
   
 </style>
-
-const element = document.getElementById('#bar-chart');
-        const margin = {top: 20, right: 10, bottom: 20, left: 10};
-        const width = 960 - margin.left - margin.right,
-        height = 500 - margin.top - margin.bottom;
-
-        var svg = d3.select("bar-chart").append("svg")
-          .attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-        .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-        var xScale = d3.scaleLinear().range([])
--- components/LACityParkingData.vue
+++ components/LACityParkingData.vue
@@ -46,3 +46,28 @@ import { eventBus } from '~/plugins/eventBus.js'
 <style>
   
 </style>
+
+        var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        // Counts number of arrests by age.
+        var countAges = alasql('SELECT INDEX age, COUNT(*) AS cnt FROM ? GROUP BY age', [data]);
+        // Counts area where arrest was made
+        var countArea = alasql('SELECT INDEX area_desc, COUNT(*) AS cnt FROM ? GROUP BY area_desc', [data]);
+        // Counts type of crime ie, dependant (d), felony (f), infraction (i), misdemeanor (m), other (o)
+        var countCrimeType = alasql('SELECT INDEX arst_typ_cd, COUNT(*) AS cnt FROM ? GROUP BY arst_typ_cd', [data]);
+        // counts Crime Description
+        var countChargeDesc = alasql('SELECT INDEX chrg_desc, COUNT(*) AS cnt FROM ? GROUP BY chrg_desc', [data]);
+        // Counts race ethnicity, origin
+        var countDemographics = alasql('SELECT INDEX descent_cd, COUNT(*) AS cnt FROM ? GROUP BY descent_cd', [data]);
+        // Counts Crime by Category
+        var countCrimeByGroupDesc = alasql('SELECT INDEX grp_description, COUNT(*) AS cnt FROM ? GROUP BY grp_description', [data]);
+        // Counts Sex (Male / Female)
+        var countSex = alasql('SELECT INDEX sex_cd, COUNT(*) AS cnt FROM ? GROUP BY sex_cd', [data]);
+        // Counts time of arrest, **Look into grouping by category
+        var countTime = alasql('SELECT INDEX time, COUNT(*) AS cnt FROM ? GROUP BY time', [data]);
+        console.log(countAges);
+        console.log(countArea);
+        console.log(countCrimeType);
+        console.log(countChargeDesc);
+        console.log(countDemographics);
+        console.log(countSex);
+        console.log(countTime);
